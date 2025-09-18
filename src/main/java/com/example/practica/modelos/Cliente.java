dffb445b4a71027969a4fec7bbea3e8b0c176ceb
YESSPER13 <YESSPER13@gmail.com>
2025-09-17 21:43:45 -0500

    se agrega modelo de cliente listo para la relacion con usuario

--- src/main/java/com/example/practica/modelos/Cliente.java
+++ src/main/java/com/example/practica/modelos/Cliente.java
@@ -1,6 +1,24 @@
 package com.example.practica.modelos;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
-    Integer id;
-    String 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    
+    public Cliente() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/com/example/practica/modelos/Vendedor.java
@@ -0,0 +1,6 @@
+package com.example.practica.modelos;
+
+public class Vendedor {
+
+
+}
